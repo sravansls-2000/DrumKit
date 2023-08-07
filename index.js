1f4a0037aad67615ce2909498546e3ffb7a12396
sravansls-2000 <sravanpadeti147@gmail.com>
2023-08-07 14:33:39 +0530

    drumkit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+const Buttons= document.querySelectorAll(".drum");
+for(var i=0 ;i<numberOfButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonHtml = this.innerHTML;
+      makeSound(buttonHtml)
+    
+    })
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var buttonHtml = this.innerHTML;
+  makeSound(buttonHtml)
+
+})
+}
+
+document.addEventListener('keydown',function(e){
+var keyPress =e.key;
+makeSound(keyPress);
+})
+
+
+function makeSound(key) {
+   switch (key) {
+    case "w":
+        var tom1 = new Audio('./sounds/tom-1.mp3');
+        tom1.play();
+        break;
+    case "a":
+        var tom2 = new Audio('./sounds/tom-2.mp3');
+        tom2.play();
+        break;  
+    case "s":
+        var tom3 = new Audio('./sounds/tom-3.mp3');
+        tom3.play();
+        break; 
+    case "d":
+        var tom4 = new Audio('./sounds/tom-4.mp3');
+        tom4.play();
+        break;
+    case "j":
+         var snare = new Audio('./sounds/snare.mp3');
+         snare.play();
+         break;
+    case "k":
+            var  crash= new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+    case "l":
+         var kickBass = new Audio('./sounds/kick-bass.mp3');
+         kickBass.play();
+                break;
+    default:console.log(innerHTML)
+        break;
+   }
+}
+
+
+
+
+
+
